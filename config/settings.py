8c0e86ed8251466d2b0b7d614d1b237851ddea86
shohrukhyakhyoev <shohrukhyakhyoev@gmail.com>
2023-03-04 10:00:06 +0500

    debug changed to false

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-isp%ckhwp^zs@)p=_-q!mb0^p87e+*4)f-75b=4=w$#$uy)f^3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['htpps://acsupport.inha.uz']
 
 
 
